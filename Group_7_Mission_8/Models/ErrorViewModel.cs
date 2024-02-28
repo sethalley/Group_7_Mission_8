887eede3261cd1be829c9c59a08676c0a0e32fab
Seth Alley <salley32@byu.edu>
2024-02-28 13:11:37 -0700

    Add project files.

---
+++ Group_7_Mission_8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group_7_Mission_8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
